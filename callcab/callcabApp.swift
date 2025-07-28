41e5bd0e32c1595e8ab3ccda49df1b580175c00c
venzox <hakanmeldakaba@icloud.com>
2025-07-28 16:52:53 +0300

    Initial Commit

---
+++ callcab/callcabApp.swift
@@ -0,0 +1,17 @@
+//
+//  callcabApp.swift
+//  callcab
+//
+//  Created by Hakan Kaba on 28.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct callcabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
